00fbda1c0f049224ae694a488d90559c20b8a1e1
HeikkeB <vldbltn@gmail.com>
2023-01-19 23:11:20 +0300

    feat: make Techs component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutProject from '../AboutProject/AboutProject';
 import Promo from '../Promo/Promo';
+import Techs from '../Techs/Techs';
 
 export default function Main() {
   return (
     <main>
       <Promo />
       <AboutProject />
+      <Techs />
     </main>
   )
 }
