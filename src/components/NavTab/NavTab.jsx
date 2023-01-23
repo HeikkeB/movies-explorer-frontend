df455aa21d0fe8d6c7a2b6e542f260e6cbfad6d7
HeikkeB <vldbltn@gmail.com>
2023-01-23 20:15:03 +0300

    feat: make burger menu

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './NavTab.css'
+import burgerMenu from '../../images/burger_menu.svg'
+import burgerMenuCross from '../../images/burger_menu_cross.svg'
 
 export default function NavTab() {
+  const [isBurgerOpen, setBurgerOpen] = useState(false)
   return (
     <nav className='navtab'>
         <div className='navtab__pages'>
