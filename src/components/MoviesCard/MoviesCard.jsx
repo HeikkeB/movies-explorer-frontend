a1749a5b65fd7ec6bae2b55d43de5fc97b0b6baf
HeikkeB <vldbltn@gmail.com>
2023-01-25 03:29:49 +0300

    feat: make movieCard components

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
+import imgCard from '../../images/img_card.png'
+import './MoviesCard.css'
+
 
 export default function MoviesCard() {
+  const [like, setLike] = useState(false)
   return (
-    <section></section>
+    <li className='movies-card'>
+      <div className='movies-card__container'>
+        <img className='movies-card__img' src={imgCard} alt={imgCard.nameRU}/>
+        <div className='movies-card__info'>
+          <div className='movies-card__info-text'>
+            <h3 className='movies-card__title'>{'Бег это свобода'}</h3>
+            <p className='movies-card__duration'>{'1ч 44м'}</p>
+          </div>
+          <button onClick={()=> setLike(!like)} className={ like ? 'movies-card__like_active' : 'movies-card__like' } type='button' aria-label="Нравится">
+          </button>
+        </div>
+      </div>
+    </li>
   )
 }
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -3,9 +3,14 @@ import MoviesCard from '../MoviesCard/MoviesCard'
 import './MoviesCardList.css'
 
 export default function MoviesCardList() {
+  const [ showMovie, setShowMovie ] = React.useState([1, 2, 3, 4, 5, 6, 7, 8, 9])
   return (
-    <section className='movies-cardlist'>
-        <MoviesCard />
+    <section className='movies-card-list'>
+    <ul className='movies-card-list__list'>
+      {showMovie.map((movie, index) => (
+        <MoviesCard key={index} movie={movie} />
+      ))}
+    </ul>
     </section>
   )
 }
