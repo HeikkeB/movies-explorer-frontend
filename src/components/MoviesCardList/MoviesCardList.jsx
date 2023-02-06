dbf6ca65f04b38b983c00ea6084eed2db74589f2
HeikkeB <vldbltn@gmail.com>
2023-02-06 17:13:11 +0300

    feat: make more btn

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -4,7 +4,7 @@ import MoviesCard from '../MoviesCard/MoviesCard'
 import './MoviesCardList.css'
 
 export default function MoviesCardList({ savedMoviesList, moviesList }) {
-  const [ showMovieList, setShowMovieList ] = useState([])
+  const [ showMoviesList, setShowMoviesList ] = useState([])
   const [showDetails, setShowDetails] = useState({ total: 12, more: 3 })
 
   const location = useLocation()
@@ -12,21 +12,32 @@ export default function MoviesCardList({ savedMoviesList, moviesList }) {
   useEffect(() => {
     if(moviesList.length) {
       const res = moviesList.filter((item, i) => i < showDetails.total)
-      setShowMovieList(res)
+      setShowMoviesList(res)
     }
 
   }, [moviesList, showDetails.total]);
 
+  function handleMoreBtn() {
+    const start = showMoviesList.length
+    const end = start + showDetails.more
+    const more = moviesList.length - start
+
+    if(more > 0) {
+      const newMovies = moviesList.slice(start, end)
+      setShowMoviesList([...showMoviesList, ...newMovies])
+    }
+  }
+
   return (
     <section className='movies-card-list'>
     <ul className='movies-card-list__list'>
-      {showMovieList.map((movie) => (
+      {showMoviesList.map((movie) => (
         <MoviesCard key={movie.id || movie._id} movie={movie} />
       ))}
     </ul>
     {
       location.pathname === '/movies' ? (
-        <button className='movies__yet-btn animation-btn'>Ещё</button>
+        <button className='movies__yet-btn animation-btn' onClick={handleMoreBtn}>Ещё</button>
       ) : (undefined)   
     }
     
